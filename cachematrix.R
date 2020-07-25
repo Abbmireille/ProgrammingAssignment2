85cba548b09382ce867ac8d519459d4d4e95f245
unknown <abmiraa25@gmail.com>
2020-07-25 20:22:13 +0300

    edit cachematrix in assignmnent2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I have created a makeCacheMatrix  function which  will  cashe
+## the inverse of the matrix that you will input  
+ 
 
-## Write a short comment describing this function
+## YOu can input any matrix and it will cashe the inverse of this matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+  
+>makeCacheMatrix<-function(x=Matrix()){
+           inv<-NULL
+           set<-function(y){
+           x<<-y
+         inv<<-NULL
+           }
+      get<-function()x
+      setInverse<-function(inverse)
+      inv<<-inverse
+      getInverse<-function()inv
+      list(set=set,
+           get=get,
+           setInverse=setInverse,
+           getInverse=getInverse)
+           }
 
-}
 
 
-## Write a short comment describing this function
+## Here i've got cacheSolve function which will compute
+## the inverse of the "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), 
+##then the cachesolve will retrieve the inverse from the cache
+ 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+>cacheSolve<-function(x,...){
+      inv<-x$getInverse()
+      if(!is.null(inv)){
+      message("getting cached data")
+      return(inv)
+          }
+       mat<-x$get()
+       inv<-solve(mat,...)
+       x$setInverse(inv)
+       inv
+          }
+
+
+
+
